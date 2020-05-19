e40044b9343f277e57936f5835e41cef95c62acb
praveengadiyaram369 <praveengadiyaram@gmail.com>
2020-05-19 12:28:26 +0200

    hackerrank submission 002

---
+++ hackerrank_002.py
@@ -0,0 +1,39 @@
+# _2D Array - DS
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+
+
+def hourglassSum(arr):
+    max_sum = -100
+    m = len(arr)
+    n = len(arr[0])
+
+    for i in range(1, m-1):
+        for j in range(1, n-1):
+            max_sum = max(max_sum, (arr[i][j] + arr[i-1][j-1] + arr[i-1][j] +
+                                    arr[i-1][j+1] + arr[i+1][j-1] + arr[i+1][j]+arr[i+1][j+1]))
+
+    return max_sum
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
